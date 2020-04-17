de3ca2a02d82ea93459d466c310918a81848e06d
wsmzjzz <wsmzjzz@gmail.com>
2020-04-17 22:55:48 +0800

    4 Modes: Commented

--- Wechat-BLE-OLED.cpp
+++ Wechat-BLE-OLED.cpp
@@ -8,18 +8,18 @@
 #define OLED_RESET 4
 Adafruit_SSD1306 oled(128, 64, &Wire, OLED_RESET);
 
-int group = 1;
-int lb = 5;
-int RM = 10;
+int group = 1; // 训练组数
+int lb = 5;    // weight
+int RM = 10;   // Repetition
 int totalTime = 0;
-int workoutTime = 0;
-int leftBtn = 12;
-int rightBtn = 11;
-int okBtn = 10;
-boolean leftRlsed = true;
+int workoutTime = 0; // each training time
+int leftBtn = 12;    // minus- :
+int rightBtn = 11;   // plus+ :
+int okBtn = 10;      // 
+boolean leftRlsed = true;  // ?Released
 boolean rightRlsed = true;
 boolean okRlsed = true;
-int oledState = 000;
+int oledState = 000; // 000:Menu 111:BreakTime 222:Training 444:End
 
 /**
  * 0: 
@@ -44,6 +44,7 @@ void output(int size, int x, int y, String str)
     oled.print(str);
     //    oled.display();
 }
+// 按钮按下-电位变低
 boolean pressed(int btn)
 {
     return digitalRead(btn) == LOW;
@@ -53,13 +54,14 @@ void setup()
 {
     Serial.begin(9600);
 
+    // init of buttons' pins
     pinMode(leftBtn, INPUT);
     digitalWrite(leftBtn, HIGH);
     pinMode(rightBtn, INPUT);
     digitalWrite(rightBtn, HIGH);
     pinMode(okBtn, INPUT);
     digitalWrite(okBtn, HIGH);
-
+    // OLED init
     oled.begin(SSD1306_SWITCHCAPVCC, 0x3C);
     oled.setTextColor(WHITE); //开像素点发光
     oled.clearDisplay();      //清屏
@@ -70,8 +72,9 @@ void setup()
 
 void loop()
 {
-    // issue：okBtn go derictly into Train Mode
+    // Menu
     if (oledState == 000) {
+        // Press any key to 'BreakTime'
         if (pressed(leftBtn) || pressed(rightBtn) || pressed(okBtn)) {
             oledState = 111;
             // delay(200);
@@ -82,8 +85,10 @@ void loop()
         // delay(1500);
 
     }
+    // BreakTime
     if (oledState == 111)
     {
+        // Combination key shortcut to 'End': -&ok | +&ok
         if (pressed(leftBtn) && leftRlsed && pressed(okBtn) ||
             pressed(rightBtn) && rightRlsed && pressed(okBtn)) {
                 oledState = 444;
@@ -93,14 +98,14 @@ void loop()
                 oled.display();
                 delay(1000);
         }
-        // 准备转到训练模式
+        // to 'Training'
         if (pressed(okBtn))
         {
             oledState = 222; // 训练模式
             workoutTime = 0; // 单次/一组训练时间
             ++group;         // 组数
 
-            // 倒计时3 2 1
+            // 倒计时3 2 1 GO
             oled.clearDisplay(); //清屏
             output(4, 60, 20, "3");
             oled.display();
@@ -121,22 +126,22 @@ void loop()
             // delay(200);
             goto NEXT;
         }
-        if (pressed(leftBtn) && leftRlsed)
+        if (pressed(leftBtn) && leftRlsed) // pressed again
         {
             leftRlsed = false;
             --RM;
-            if (RM < 1) RM = 1;
+            if (RM < 1) RM = 1; // min:1
         }
         if (!pressed(leftBtn))
         {
             leftRlsed = true;
         }
 
-        if (pressed(rightBtn) && rightRlsed)
+        if (pressed(rightBtn) && rightRlsed) // pressed again
         {
             rightRlsed = false;
             ++RM;
-            if (RM > 25) RM = 25;
+            if (RM > 25) RM = 25; // max:25
         }
         if (!pressed(rightBtn))
         {
@@ -149,12 +154,12 @@ void loop()
 
         oled.display(); // 开显示
     }
-    if (oledState == 222) // Training
+    if (oledState == 222) // Training Mode
     {
-        if (pressed(okBtn)) // to Break
+        if (pressed(okBtn)) // to 'Break'
         {
             oledState = 111;
-
+            // Statistic
             totalTime += workoutTime;
             workoutTime = 0;
 
@@ -165,42 +170,40 @@ void loop()
             goto NEXT;
         }
         oled.clearDisplay(); //清屏
-
+        // Gx Rxx
         output(2, 0, 0, "G");
         output(2, 0 + 13, 0, group);
-
         output(2, 45, 0, "R");
         output(2, 45 + 13, 0, RM);
-
-        // String s = "hello";
+        // time x'xx
         output(2, 0, 30, "time");
         ++workoutTime;
         output(2, 60, 30, workoutTime / 60);
         output(2, 60 + 13, 30, "'");
         output(2, 60 + 26, 30, workoutTime % 60);
 
-
         oled.display(); // 开显示
 
-        delay(1000);
+        delay(1000); // time counting
     }
+    // End
     if (oledState == 444) {
+        // Press any key to 'Menu' 
         if (pressed(leftBtn) || pressed(rightBtn) || pressed(okBtn)) {
             oledState = 000;
-
+            // End words
             oled.clearDisplay();      //清屏
             output(2, 0, 0, "Keep it up & Go for it");
             oled.display();
             delay(1500);
             // delay(200);
         }
-
+        // End statistics
         oled.clearDisplay(); //清屏
         output(2, 0, 0, "Time:");
         output(2, 30, 30, totalTime / 60);
         output(2, 30 + 13, 30, "'");
         output(2, 30 + 26, 30, totalTime % 60);
-
         oled.display();
         // delay(1000);
 
